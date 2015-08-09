3ba9f6a1b065cb87d2361200e7e935ccab3e921d
AziasCreations <herwin0996@gmail.com>
2015-08-09 17:05:37 +0200

    1.0.1 Update
    Added missing recipes

--- src/main/java/com/azias/vendingmachine/VendingMachineMod.java
+++ src/main/java/com/azias/vendingmachine/VendingMachineMod.java
@@ -19,7 +19,7 @@ public class VendingMachineMod {
 	private static VendingMachineEvents modEvents = new VendingMachineEvents();
     public static final String MODID = "aziasvendingmachine";
     public static final String MODNAME = "Vending Machines Revamped";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 	public static String VERSIONOFFICIAL;
     public static CreativeTabs tabVendingMachines = new VendingMachineCreativeTab("VendingMachines");
     
--- src/main/java/com/azias/vendingmachine/VendingMachineRecipes.java
+++ src/main/java/com/azias/vendingmachine/VendingMachineRecipes.java
@@ -17,31 +17,75 @@ public class VendingMachineRecipes {
 		    'A', Blocks.wooden_button,
 		    'B', new ItemStack(Items.potionitem, 1, 0)});
 		
+		//Cola
 		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 1), new Object[] {
 			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
 			new ItemStack(Items.dye, 1, 3),
 			Items.sugar,
 			Items.sugar});
 		
+		//Diet Cola
 		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 2), new Object[] {
 			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
 			new ItemStack(Items.dye, 1, 3),
 			Items.sugar});
 		
+		//Caffeine Free Cola
 		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 3), new Object[] {
 			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
 			Items.sugar,
 			Blocks.dirt});
 		
+		//Cola Zero
 		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 4), new Object[] {
 			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
 			new ItemStack(Items.dye, 1, 3),
 			Items.reeds});
 		
-		/*GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 5), new Object[] {
+		//Cherry Cola
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 6), new Object[] {
 			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
 			new ItemStack(Items.dye, 1, 3),
-			Items.bone,});*/
+			Items.sugar,
+			Items.apple});
+		
+		//Fanta
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 7), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			new ItemStack(Items.dye, 1, 14),
+			Items.sugar,
+			Items.apple});
+		
+		//Mountain Dew
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 8), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			new ItemStack(Items.dye, 1, 10),
+			Items.sugar,
+			Items.golden_apple});
+		
+		//Sprite
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 9), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			Items.sugar});
+		
+		//Sprite Zero
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 10), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			Items.reeds});
+		
+		//Powerade
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 11), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			new ItemStack(Items.dye, 1, 4),
+			Items.sugar,
+			Items.golden_apple});
+		
+		//Fruit Juice
+		GameRegistry.addShapelessRecipe(new ItemStack(VendingMachineItems.sodaBottle, 1, 12), new Object[] {
+			new ItemStack(VendingMachineItems.sodaBottle, 1, 0),
+			Items.apple,
+			Items.sugar,
+			Items.carrot});
 		
 		/* Candies */
 		GameRegistry.addRecipe(new ItemStack(VendingMachineItems.candy, 1, 0), new Object[]{
